f8a0dfa67f6d443bffaf23d8a916b2152a23b2bf
Bernardo Andrade <bernardo.andrade@engenharia.ufjf.br>
2023-06-28 16:19:46 -0300

    Leitura da valvula OK, FP leitura errada

--- Python/Trabalho Final/Entrega Parcial 1/ClienteMODBUS/main.py
+++ Python/Trabalho Final/Entrega Parcial 1/ClienteMODBUS/main.py
@@ -15,52 +15,54 @@ class MyWidget(BoxLayout):  # é o raiz da aplicação
         ip_server = self.ids.ip.text
         port_server = int(self.ids.port.text)
         self._cliente = ModbusClient(host=ip_server, port=port_server)
-        self._scan_time = 1./10.
+        self._scan_time = 1.0
         self._cliente.open()
         self.ids.titulo_conectado.text = "Conexão estabelecida"
         self._ev = Clock.schedule_interval(self.atendimentoRepeat, self._scan_time)
-    
-    def leDadoHolding(self,addr,div):
-        leitura_noDiv = self._cliente.read_holding_registers(addr,1)[0]
-        leitura = int(leitura_noDiv)/div
-        return str(leitura)
-    
-    def leDadoFP(self,addr):
-        leitura_cod = self._cliente.read_holding_registers(addr,2)
-        decoder = BinaryPayloadDecoder.fromRegisters(leitura,byteorder=Endian.Big,wordorder=Endian.Big)
-        leitura = str(decoder.decode_32bit_float())
-        return leitura
-    
-    def leDadoValvula(self,addr,bit):
-        leitura = self._cliente.read_holding_registers(addr,1)[0]
-        lista_bits = [int(x) for x in '{0:016b}'.format(leitura)]
-        return str(lista_bits[bit])
-    
-    def checkValvula(self,valvula):
-        if(valvula == '1'):
-            valvula = 'Fechada'
+
+    def leDadoHolding(self, addr, div):
+        leitura_noDiv = self._cliente.read_holding_registers(addr, 1)[0]
+        leitura_Holding = int(leitura_noDiv) / div
+        return str(leitura_Holding)
+
+    def leDadoFP(self, addr):
+        leitura_cod = self._cliente.read_holding_registers(addr, 2)
+        decoder = BinaryPayloadDecoder.fromRegisters(
+            leitura_cod, byteorder=Endian.Big, wordorder=Endian.Big
+        )
+        leitura_FP = str(round(decoder.decode_32bit_float(), 2))
+        return leitura_FP
+
+    def leDadoValvula(self, addr, bit):
+        leitura_v = self._cliente.read_holding_registers(addr, 1)[0]
+        lista_bits = [int(x) for x in "{0:016b}".format(leitura_v)]
+        return str(lista_bits[abs(15 - bit)])
+
+    def checkValvula(self, valvula):
+        if valvula == "1":
+            valvula = "Aberta"
             return valvula
-        elif (valvula == '0'):
-            valvula = 'Aberta'
+        elif valvula == "0":
+            valvula = "Fechada"
             return valvula
 
     def atendimentoRepeat(self, dt):
         try:
-            valvula1 = self.checkValvula(self.leDadoValvula(712,0))
-            valvula2 = self.checkValvula(self.leDadoValvula(712,1))
-            valvula3 = self.checkValvula(self.leDadoValvula(712,2))
-            valvula4 = self.checkValvula(self.leDadoValvula(712,3))
-            valvula5 = self.checkValvula(self.leDadoValvula(712,4))
-            valvula6 = self.checkValvula(self.leDadoValvula(712,5))
-            
+            valvula1 = self.checkValvula(self.leDadoValvula(712, 0))
+            valvula2 = self.checkValvula(self.leDadoValvula(712, 1))
+            valvula3 = self.checkValvula(self.leDadoValvula(712, 2))
+            valvula4 = self.checkValvula(self.leDadoValvula(712, 3))
+            valvula5 = self.checkValvula(self.leDadoValvula(712, 4))
+            valvula6 = self.checkValvula(self.leDadoValvula(712, 5))
+
             pit_01 = self.leDadoFP(714)
             fit_02 = self.leDadoFP(716)
             fit_03 = self.leDadoFP(718)
-            
+
             torque = self.leDadoFP(1420)
             velocidade = self.leDadoFP(884)
-            corrente_med = self.leDadoHolding(845,10)
-            
+            corrente_med = self.leDadoHolding(845, 10)
+
             self.ids.leituraUm.text = valvula1
             self.ids.leituraDois.text = valvula2
             self.ids.leituraTres.text = valvula3
@@ -72,8 +74,8 @@ class MyWidget(BoxLayout):  # é o raiz da aplicação
             self.ids.leituraNove.text = fit_03
             self.ids.leituraDez.text = torque
             self.ids.leituraOnze.text = velocidade
-            self.ids.leituraDoze.text = corrente_med          
-            
+            self.ids.leituraDoze.text = corrente_med
+
         except Exception as e:
             print("Erro no atendimento: ", e.args)
 
