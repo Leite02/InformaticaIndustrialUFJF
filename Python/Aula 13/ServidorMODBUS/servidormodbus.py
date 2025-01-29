4aea5fee71b8d6959f495de496e6fbe14d22f76a
Leite02 <leiteferreiraarthur@gmail.com>
2025-01-29 16:44:43 -0300

    ok

--- Python/Aula 13/ServidorMODBUS/servidormodbus.py
+++ Python/Aula 13/ServidorMODBUS/servidormodbus.py
@@ -13,8 +13,10 @@ class ServidorMODBUS():
         Construtor
         """
         self._db = DataBank() #objeto que guarda a tabela de dados do modbus, bobinas, etc etc
-        self._server = ModbusServer(host=host_ip,port=port,no_block=True,data_bank=self._db) #cria objeto servidor com todas subrotinas do MODBUS (parecido com objeto da api socket)
-       #além disso, adicionamos o no_block = True (já cria servidor MultiThread, atende múltiplos clientes) e passamos o databank criado
+        self._server = ModbusServer(host=host_ip,port=port,no_block=True,data_bank=self._db) #cria objeto servidor 
+        #com todas subrotinas do MODBUS (parecido com objeto da api socket)(não precisa referenciar bind etc...)
+        #além disso, adicionamos o no_block = True (já cria servidor MultiThread, atende múltiplos clientes)
+        #e passamos o databank criado
         
     def run(self):  #método START
         """
@@ -24,7 +26,7 @@ class ServidorMODBUS():
             self._server.start() #da start
             print("Servidor MODBUS em execução")
             while True: #vai colocar dados randomicos (como se fosse o clp com dados armazenados)
-                self._db.set_holding_registers(1000,[random.randrange(int(0.95*400),int(1.05*400))]) #coloca no holding de endereço 1000 um valor random
+                self._db.set_holding_registers(1000,[random.randrange(int(0.95*400),int(1.05*400))]) #coloca no holding de endereço 1000 um valor random, que varia nesse caso de 380 a 420
                 print('======================')
                 print("Tabela MODBUS")
                 print(f'Holding Register \r\n R1000: {self._db.get_holding_registers(1000)} \r\n R2000: {self._db.get_holding_registers(2000)}') #le valores do holding nos endereços 1000 e 2000
